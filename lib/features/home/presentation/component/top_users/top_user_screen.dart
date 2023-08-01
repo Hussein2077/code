15fcf89fd9b594851d0140f0975f5ecb0994a437
Ahmed Aly <57324624+ahmed3670-art@users.noreply.github.com>
2023-08-01 11:31:05 +0300

    add

--- lib/features/home/presentation/component/top_users/top_user_screen.dart
+++ lib/features/home/presentation/component/top_users/top_user_screen.dart
@@ -7,7 +7,6 @@ import 'package:tik_chat_v2/core/widgets/screen_color_back_ground.dart';
 import 'package:tik_chat_v2/features/home/presentation/component/top_users/widget/top_users_type_body.dart';
 import 'package:tik_chat_v2/features/home/presentation/component/top_users/widget/type_tabs.dart';
 
-
 class TopUsersScreen extends StatefulWidget {
   const TopUsersScreen({super.key});
 
@@ -15,51 +14,50 @@ class TopUsersScreen extends StatefulWidget {
   State<TopUsersScreen> createState() => _TopUsersScreenState();
 }
 
-class _TopUsersScreenState extends State<TopUsersScreen> with TickerProviderStateMixin {
-  late TabController typeController ; 
-  late TabController userTimeController ; 
-    late TabController hostTimeController ; 
-
+class _TopUsersScreenState extends State<TopUsersScreen>
+    with TickerProviderStateMixin {
+  late TabController typeController;
+  late TabController userTimeController;
+  late TabController hostTimeController;
 
   @override
   void initState() {
-    typeController = TabController(length: 2 , vsync: this);
-    userTimeController = TabController(length: 3 , vsync: this);
-        hostTimeController = TabController(length: 3 , vsync: this);
+    typeController = TabController(length: 2, vsync: this);
+    userTimeController = TabController(length: 3, vsync: this);
+    hostTimeController = TabController(length: 3, vsync: this);
 
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: ScreenColorBackGround(
-        color: ColorManager.mainColorList,
-        child: Column(
-          children:  [
-  SizedBox(height: ConfigSize.defaultSize!*3,),           
-const HeaderWithOnlyTitle(title: StringManager.rank),
- TypeTabs(typeController: typeController,),
-   SizedBox(height: ConfigSize.defaultSize!*3,),           
-
- Expanded(
-   child: TabBarView(
-    controller: typeController,
-    children: [
-       TopUsersTypeBody(timeController: hostTimeController,),
-        TopUsersTypeBody(timeController: userTimeController,)
- 
- 
-    
-   ]),
- )
-
-
-
-
-
-
- 
-
-          ],
-        )),);
+    return Scaffold(
+      body: ScreenColorBackGround(
+          color: ColorManager.mainColorList,
+          child: Column(
+            children: [
+              SizedBox(
+                height: ConfigSize.defaultSize! * 3,
+              ),
+              const HeaderWithOnlyTitle(title: StringManager.rank),
+              TypeTabs(
+                typeController: typeController,
+              ),
+              SizedBox(
+                height: ConfigSize.defaultSize! * 3,
+              ),
+              Expanded(
+                child: TabBarView(controller: typeController, children: [
+                  TopUsersTypeBody(
+                    timeController: hostTimeController,
+                  ),
+                  TopUsersTypeBody(
+                    timeController: userTimeController,
+                  )
+                ]),
+              )
+            ],
+          )),
+    );
   }
 }
