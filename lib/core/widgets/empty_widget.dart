8425d3515356f42748d83fa5435de13b2e6cbf66
R star <75912765+ohamedkhaled@users.noreply.github.com>
2023-08-22 10:35:34 +0300

    some edite2

--- lib/core/widgets/empty_widget.dart
+++ lib/core/widgets/empty_widget.dart
@@ -1,41 +1,27 @@
 import 'package:flutter/material.dart';
-
-import 'package:tik_chat_v2/core/resource_manger/asset_path.dart';
 import 'package:tik_chat_v2/core/utils/config_size.dart';
-import 'package:tik_chat_v2/features/room/presentation/components/profile/user_porfile_in_room_body.dart';
-import 'package:tik_chat_v2/core/widgets/bottom_dailog.dart';
 
 class EmptyWidget extends StatelessWidget {
   final String message;
+  final double? height;
+  const EmptyWidget({required this.message, super.key, this.height,  });
 
-  const EmptyWidget({required this.message, super.key});
+@override
+Widget build(BuildContext context) {
+  return
+    Center(
+      child: Container(
+        height: ConfigSize.screenHeight!*0.825,
+        alignment: Alignment.center,
+        color: Colors.white,
+        child: Text(
+          message,
+          style: TextStyle(
+              color: Colors.black, fontSize: ConfigSize.defaultSize! * 2),
+        ),
+      ),
+    )
 
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-        child: InkWell(
-          onTap: (){
-            bottomDailog(
-                context: context,
-                widget: const UserProfileInRoom()
-            );
-          },
-          child: Stack(
-      alignment: Alignment.center,
-      children: [
-          SizedBox(
-            child: Image.asset(AssetsPath.emptyScreen),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: ConfigSize.defaultSize! * 20),
-            child: Text(
-              message,
-              style: TextStyle(
-                  color: Colors.black, fontSize: ConfigSize.defaultSize! * 2),
-            ),
-          ),
-      ],
-    ),
-        ));
-  }
+  ;
+}
 }
