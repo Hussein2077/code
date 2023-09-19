6d3527ccdaff9eb12b77acb4620c3115a06cf0a0
Ahmed Aly <57324624+ahmed3670-art@users.noreply.github.com>
2023-09-19 14:13:41 +0300

    add

---
+++ lib/features/profile/persentation/manager/manager_theme/theme_state.dart
@@ -0,0 +1,15 @@
+
+import 'package:equatable/equatable.dart';
+
+abstract class ThemeState extends Equatable {
+  const ThemeState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+ class ThemeInitial extends ThemeState {}
+
+class LightThemeState extends ThemeState{}
+
+class DarkThemeState extends ThemeState{}
