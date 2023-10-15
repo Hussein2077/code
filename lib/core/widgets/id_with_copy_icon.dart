63af0c8712c58607d58794710bc1786f44983d12
Ahmed sameh <76152669+ahmed5060@users.noreply.github.com>
2023-10-15 18:51:27 +0300

    add is gold in user data model

--- lib/core/widgets/id_with_copy_icon.dart
+++ lib/core/widgets/id_with_copy_icon.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:tik_chat_v2/core/model/my_data_model.dart';
 import 'package:tik_chat_v2/core/resource_manger/color_manager.dart';
 import 'package:tik_chat_v2/core/resource_manger/string_manager.dart';
 import 'package:tik_chat_v2/core/utils/config_size.dart';
@@ -24,7 +25,7 @@ class IdWithCopyIcon extends StatelessWidget {
         children: [
           Text('ID: $id',
               style: TextStyle(
-                  color: ColorManager.orang,
+                  color: MyDataModel.getInstance().isGold! ? ColorManager.gold : ColorManager.orang,
                   fontSize: ConfigSize.defaultSize! * 1.7)),
           SizedBox(
             width: ConfigSize.defaultSize,
