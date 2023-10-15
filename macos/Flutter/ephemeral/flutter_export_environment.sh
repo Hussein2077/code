9af6eea510ce493e83546b14c81a38e8c2e5d70d
R star <75912765+ohamedkhaled@users.noreply.github.com>
2023-10-15 10:10:49 +0300

    add

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mac/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mac/GitHub/tikChatV2-"
+export "FLUTTER_ROOT=C:\Program Files\flutter"
+export "FLUTTER_APPLICATION_PATH=F:\tikChatV2-"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.2.9"
-export "FLUTTER_BUILD_NUMBER=1.2.9"
+export "FLUTTER_BUILD_NAME=1.3.0"
+export "FLUTTER_BUILD_NUMBER=1.3.0"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
