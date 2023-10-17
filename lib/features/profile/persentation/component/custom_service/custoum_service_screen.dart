06670a0bdc912c103db894b8f667f0a15518f6b0
Ahmed sameh <76152669+ahmed5060@users.noreply.github.com>
2023-10-17 18:41:27 +0300

    edit complaints screen

--- lib/features/profile/persentation/component/custom_service/custoum_service_screen.dart
+++ lib/features/profile/persentation/component/custom_service/custoum_service_screen.dart
@@ -41,103 +41,103 @@ class _CustoumServiceScreenState extends State<CustoumServiceScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: ConfigSize.defaultSize!,
-              ),
-              HeaderWithOnlyTitle(title: StringManager.complaints.tr()),
-              Text(
-                StringManager.typeOfProblem.tr(),
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              const ProblemType(),
-              Text(
-                StringManager.details.tr(),
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width - 50,
-                padding:
-                    EdgeInsets.symmetric(horizontal: ConfigSize.defaultSize!),
-                decoration: BoxDecoration(
-                    color: ColorManager.lightGray,
-                    borderRadius:
-                        BorderRadius.circular(ConfigSize.defaultSize! * 2)),
-                child: TextFieldWidget(
-                    maxLines: 4,
-                    hintText: StringManager.explainProblem.tr(),
-                    controller: detailsController),
-              ),
-              Text(
-                StringManager.whatsappNum.tr(),
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width - 50,
-                padding:
-                    EdgeInsets.symmetric(horizontal: ConfigSize.defaultSize!),
-                decoration: BoxDecoration(
-                    color: ColorManager.lightGray,
-                    borderRadius:
-                        BorderRadius.circular(ConfigSize.defaultSize! * 2)),
-                child: TextFieldWidget(
-                    type: TextInputType.number,
-                    hintText: "",
-                    controller: contactController),
-              ),
-              Text(
-                StringManager.screenshot.tr(),
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              const AddScreenShot(),
-              BlocConsumer<FeedBackBloc, FeedBackState>(
-                listener: (context, state) {
-                  if (state is FeedBackSucssesState) {
-                    sucssesToast(context: context, title: state.massage);
-                  } else if (state is FeedBackErrorState) {
-                    errorToast(context: context, title: state.error);
-                  }
-                },
-                builder: (context, state) {
-                  if (state is FeedBackSucssesState) {
-                    return const SizedBox();
-                  } else {
-                    return MainButton(
-                        onTap: () {
-                          if (contactController.text.isEmpty) {
-                            errorToast(
-                                context: context,
-                                title: StringManager.enterPhoneNum.tr());
-                          } else if (detailsController.text.isEmpty) {
-                            errorToast(
-                                context: context,
-                                title: StringManager.pleaseAddDetiels.tr());
-                          } else if (AddScreenShot.image == null) {
-                            errorToast(
-                                context: context,
-                                title: StringManager.enterYourImage.tr());
-                          } else {
-                            BlocProvider.of<FeedBackBloc>(context).add(
-                                FeedBackEvent(
-                                    content: ProblemType
-                                        .types[ProblemType.seletedProblem],
-                                    phoneNumber: contactController.text,
-                                    description: detailsController.text,
-                                    image: File(AddScreenShot.image!.path),
-                                    userId: widget.myId));
-                          }
-                        },
-                        title: StringManager.submit.tr());
-                  }
-                },
-              )
-            ],
+      body: SafeArea(
+        bottom: false,
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * .8,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                HeaderWithOnlyTitle(title: StringManager.complaints.tr()),
+                Text(
+                  StringManager.typeOfProblem.tr(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const ProblemType(),
+                Text(
+                  StringManager.details.tr(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 50,
+                  padding:
+                      EdgeInsets.symmetric(horizontal: ConfigSize.defaultSize!),
+                  decoration: BoxDecoration(
+                      color: ColorManager.lightGray,
+                      borderRadius:
+                          BorderRadius.circular(ConfigSize.defaultSize! * 2)),
+                  child: TextFieldWidget(
+                      maxLines: 4,
+                      hintText: StringManager.explainProblem.tr(),
+                      controller: detailsController),
+                ),
+                Text(
+                  StringManager.whatsappNum.tr(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 50,
+                  padding:
+                      EdgeInsets.symmetric(horizontal: ConfigSize.defaultSize!),
+                  decoration: BoxDecoration(
+                      color: ColorManager.lightGray,
+                      borderRadius:
+                          BorderRadius.circular(ConfigSize.defaultSize! * 2)),
+                  child: TextFieldWidget(
+                      type: TextInputType.number,
+                      hintText: "",
+                      controller: contactController),
+                ),
+                Text(
+                  StringManager.screenshot.tr(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const AddScreenShot(),
+                BlocConsumer<FeedBackBloc, FeedBackState>(
+                  listener: (context, state) {
+                    if (state is FeedBackSucssesState) {
+                      sucssesToast(context: context, title: state.massage);
+                    } else if (state is FeedBackErrorState) {
+                      errorToast(context: context, title: state.error);
+                    }
+                  },
+                  builder: (context, state) {
+                    if (state is FeedBackSucssesState) {
+                      return const SizedBox();
+                    } else {
+                      return MainButton(
+                          onTap: () {
+                            if (contactController.text.isEmpty) {
+                              errorToast(
+                                  context: context,
+                                  title: StringManager.enterPhoneNum.tr());
+                            } else if (detailsController.text.isEmpty) {
+                              errorToast(
+                                  context: context,
+                                  title: StringManager.pleaseAddDetiels.tr());
+                            } else if (AddScreenShot.image == null) {
+                              errorToast(
+                                  context: context,
+                                  title: StringManager.enterYourImage.tr());
+                            } else {
+                              BlocProvider.of<FeedBackBloc>(context).add(
+                                  FeedBackEvent(
+                                      content: ProblemType
+                                          .types[ProblemType.seletedProblem],
+                                      phoneNumber: contactController.text,
+                                      description: detailsController.text,
+                                      image: File(AddScreenShot.image!.path),
+                                      userId: widget.myId));
+                            }
+                          },
+                          title: StringManager.submit.tr());
+                    }
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
