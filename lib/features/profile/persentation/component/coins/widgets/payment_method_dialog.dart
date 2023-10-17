c62c957f2512eaaf83f5e53074b62e5291c51039
Ahmed sameh <76152669+ahmed5060@users.noreply.github.com>
2023-10-17 11:02:53 +0300

    in app purchase

--- lib/features/profile/persentation/component/coins/widgets/payment_method_dialog.dart
+++ lib/features/profile/persentation/component/coins/widgets/payment_method_dialog.dart
@@ -1,116 +1,39 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, must_be_immutable
+
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'dart:async';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:tik_chat_v2/core/resource_manger/asset_path.dart';
 import 'package:tik_chat_v2/features/profile/domin/use_case/buy_coins_uc.dart';
 import 'package:tik_chat_v2/features/profile/persentation/manager/buy_coins_manger/buy_coins_bloc.dart';
 import 'package:tik_chat_v2/features/profile/persentation/manager/buy_coins_manger/buy_coins_event.dart';
-
 import '../../../../../../core/utils/config_size.dart';
 
 class PaymentMethodDialog extends StatefulWidget {
 
   var coinPackageId;
-  PaymentMethodDialog({this.coinPackageId});
+  var product;
+  InAppPurchase inAppPurchase;
+  PaymentMethodDialog({super.key, required this.coinPackageId, this.product, required this.inAppPurchase});
 
   @override
   State<PaymentMethodDialog> createState() => _PaymentMethodDialogState();
 }
 
 class _PaymentMethodDialogState extends State<PaymentMethodDialog> {
-  final InAppPurchase _inAppPurchase = InAppPurchase.instance;
 
-  final String _productID = 'test_3';
 
-  bool _available = true;
-
-  List<ProductDetails> _products = [];
-
-  List<PurchaseDetails> _purchases = [];
-
-  StreamSubscription<List<PurchaseDetails>>? _subscription;
-
-  @override
-  void initState() {
-    final Stream<List<PurchaseDetails>> purchaseUpdated = _inAppPurchase.purchaseStream;
-
-    _subscription = purchaseUpdated.listen((purchaseDetailsList) {
-      setState(() {
-        _purchases.addAll(purchaseDetailsList);
-        _listenToPurchaseUpdated(purchaseDetailsList);
-      });
-    }, onDone: () {
-      _subscription!.cancel();
-    }, onError: (error) {
-      _subscription!.cancel();
-    });
-
-    _initialize();
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _subscription!.cancel();
-    super.dispose();
-  }
-
-  void _initialize() async {
-    _available = await _inAppPurchase.isAvailable();
-
-    List<ProductDetails> products = await _getProducts(
-      productIds: Set<String>.from(
-        [_productID],
-      ),
-    );
-
-    setState(() {
-      _products = products;
-    });
-  }
-
-  void _listenToPurchaseUpdated(List<PurchaseDetails> purchaseDetailsList) {
-    purchaseDetailsList.forEach((PurchaseDetails purchaseDetails) async {
-      switch (purchaseDetails.status) {
-        case PurchaseStatus.pending:
-        //  _showPendingUI();
-          break;
-        case PurchaseStatus.purchased:
-        case PurchaseStatus.restored:
-        // bool valid = await _verifyPurchase(purchaseDetails);
-        // if (!valid) {
-        //   _handleInvalidPurchase(purchaseDetails);
-        // }
-          break;
-        case PurchaseStatus.error:
-          print(purchaseDetails.error!);
-          // _handleError(purchaseDetails.error!);
-          break;
-        default:
-          break;
-      }
-
-      if (purchaseDetails.pendingCompletePurchase) {
-        await _inAppPurchase.completePurchase(purchaseDetails);
-      }
-    });
-  }
-
-  Future<List<ProductDetails>> _getProducts({required Set<String> productIds}) async {
-    ProductDetailsResponse response = await _inAppPurchase.queryProductDetails(productIds);
-
-    return response.productDetails;
-  }
-
-  void _subscribe({required ProductDetails product}) {
+  Future<void> _buyProduct(ProductDetails product) async {
     final PurchaseParam purchaseParam = PurchaseParam(productDetails: product);
-    _inAppPurchase.buyNonConsumable(
-      purchaseParam: purchaseParam,
-    );
+    try {
+      await widget.inAppPurchase.buyConsumable(purchaseParam: purchaseParam);
+    } catch (e) {
+      log("error in buy function: $e");
+      // Handle purchase errors, if any.
+    }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -120,7 +43,7 @@ class _PaymentMethodDialogState extends State<PaymentMethodDialog> {
       shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(32.0))),
       child: Container(
         width: double.infinity,
-        height: ConfigSize.defaultSize! * 22,
+        height: ConfigSize.defaultSize! * 23,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(15),
             color: Colors.white
@@ -162,7 +85,7 @@ class _PaymentMethodDialogState extends State<PaymentMethodDialog> {
 
             InkWell(
               onTap: (){
-                _subscribe(product: _products[0]);
+                _buyProduct(widget.product);
               },
               child: Container(
                 height: ConfigSize.defaultSize! * 5,
