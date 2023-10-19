2864a436532ca237f7c75d6c24430367930eec04
jacklinekamel <totakamel2020@gmail.com>
2023-10-19 18:51:56 +0300

    jako

--- lib/features/home/data/model/carousels_model.dart
+++ lib/features/home/data/model/carousels_model.dart
@@ -1,29 +1,30 @@
 
-
 import 'package:equatable/equatable.dart';
 
 class CarouselsModel extends Equatable {
-  final int id;
-  final String img;
-  final int ownerId;
-  final bool hasPassword;
+  final int  id ;
+  final String img ;
+  final String contents ;
+  final String url ;
+
 
   const CarouselsModel({
-    required this.id,
-    required this.img,
-    required this.ownerId,
-    required this.hasPassword,
-  });
+    required  this.id, required this.img, required this.contents,required this.url});
 
-  factory CarouselsModel.fromJson(Map<String, dynamic> jsonData) {
+
+
+  factory CarouselsModel.fromJson(Map<String , dynamic > jsonData){
     return CarouselsModel(
-      id: jsonData['id'],
-      img: jsonData['img'],
-      ownerId: jsonData['owner_id'],
-      hasPassword: jsonData['isLocked'],
+        id: jsonData['id'],
+        img:jsonData['img'],
+        url:jsonData['url'],
+        contents: jsonData['contents']
     );
   }
-
   @override
-  List<Object?> get props => [id, img, ownerId,hasPassword];
+  List<Object?> get props => [id,img,contents];
+
 }
+
+
+
