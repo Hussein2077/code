ccc2fdd173b1858d342c429c41991d070164b7bf
Ahmed sameh <76152669+ahmed5060@users.noreply.github.com>
2023-10-19 12:53:06 +0300

    change pop uo sheet color

--- lib/zego_code_v2/zego_live_audio_room/src/components/pop_up_sheet_menu.dart
+++ lib/zego_code_v2/zego_live_audio_room/src/components/pop_up_sheet_menu.dart
@@ -8,6 +8,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 // Package imports:
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tik_chat_v2/core/resource_manger/color_manager.dart';
  
 import 'package:tik_chat_v2/core/widgets/bottom_dailog.dart';
 import 'package:tik_chat_v2/features/room_audio/data/model/ente_room_model.dart';
@@ -197,10 +198,10 @@ class _ZegoPopUpSheetMenuState extends State<ZegoPopUpSheetMenu> {
         decoration: BoxDecoration(
           border: (index == (widget.popupItems.length - 1))
               ? null
-              : Border(
+              : const Border(
                   bottom: BorderSide(
                     width: 0.5,
-                    color: Colors.white.withOpacity(0.2),
+                    color: Colors.white,
                   ),
                 ),
         ),
@@ -209,7 +210,7 @@ class _ZegoPopUpSheetMenuState extends State<ZegoPopUpSheetMenu> {
             popupItem.text,
             style: TextStyle(
               fontSize: 28.r,
-              fontWeight: FontWeight.w400,
+              fontWeight: FontWeight.bold,
               color: Colors.white,
             ),
           ),
@@ -246,7 +247,7 @@ void showPopUpSheet({
 
   showModalBottomSheet(
     barrierColor: ZegoUIKitDefaultTheme.viewBarrierColor,
-    backgroundColor: const Color(0xff111014),
+    backgroundColor: ColorManager.deeporang,
     //ZegoUIKitDefaultTheme.viewBackgroundColor,
     context: context,
     shape: RoundedRectangleBorder(
