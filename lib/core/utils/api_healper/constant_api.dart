184ea8eb345c31e9f780f11c5d507e49ffc3f9f8
R star <75912765+ohamedkhaled@users.noreply.github.com>
2023-11-07 10:22:15 +0200

    add

--- lib/core/utils/api_healper/constant_api.dart
+++ lib/core/utils/api_healper/constant_api.dart
@@ -6,8 +6,8 @@ import 'package:tik_chat_v2/features/room_audio/domine/use_case/up_mic_usecase.d
 
 
 class ConstentApi {
-  static const String baseUrl = 'https://tik-chat.com/api' ;
-// static const String baseUrl = 'https://test.tik-chat.com/api' ;
+ // static const String baseUrl = 'https://tik-chat.com/api' ;
+  static const String baseUrl = 'https://test.tik-chat.com/api' ;
   static const String getBoxes = "$baseUrl/box/list";
   static const String inAppPurchase = "$baseUrl/purchased";
   static const String sendBox = "$baseUrl/box/send";
