4bddbfe973424dba5be912ae2862b550fe90570e
R star <75912765+ohamedkhaled@users.noreply.github.com>
2023-12-12 19:56:52 +0200

    add

--- lib/features/room_audio/presentation/Room_Screen.dart
+++ lib/features/room_audio/presentation/Room_Screen.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: use_build_context_synchronously, depend_on_referenced_packages
-import 'dart:developer';
 import 'dart:math';
 
 import 'package:path_provider/path_provider.dart';
@@ -895,7 +894,7 @@ class RoomScreenState extends State<RoomScreen> with TickerProviderStateMixin {
                     gameId: result[messageContent]['game_record_id'].toString(),
                     answer: answer.toString()
                 )));
-                ZegoUIKit.instance.sendInRoomMessage("$answer", false,games:GamesInRoom.dicGame);
+                ZegoUIKit.instance.sendInRoomMessage("$answer", false,null);
               }
             }else{
               Navigator.pop(context);
