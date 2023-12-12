328a4ee02f6276a8f4621c6f9467c315da772582
R star <75912765+ohamedkhaled@users.noreply.github.com>
2023-12-12 17:45:29 +0200

    adds

--- lib/zego_code_v3/zego_uikit/src/components/message/message_view.dart
+++ lib/zego_code_v3/zego_uikit/src/components/message/message_view.dart
@@ -2,16 +2,14 @@
 import 'dart:async';
 
 // Flutter imports:
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:profanity_filter/profanity_filter.dart';
-import 'package:tik_chat_v2/core/resource_manger/asset_path.dart';
-import 'package:tik_chat_v2/core/resource_manger/color_manager.dart';
 import 'package:tik_chat_v2/core/resource_manger/string_manager.dart';
 import 'package:tik_chat_v2/core/utils/api_healper/enum.dart';
 import 'package:tik_chat_v2/features/room_audio/data/model/ente_room_model.dart';
 import 'package:tik_chat_v2/features/room_audio/presentation/Room_Screen.dart';
 import 'package:tik_chat_v2/features/room_audio/presentation/components/pk/pk_functions.dart';
+import 'package:tik_chat_v2/zego_code_v3/zego_live_audio/src/components/message/messgas_body.dart';
 
 // Project imports:
 import 'package:tik_chat_v2/zego_code_v3/zego_uikit/src/components/message/defines.dart';
@@ -49,10 +47,6 @@ class ZegoInRoomMessageView extends StatefulWidget {
 /// @nodoc
 class ZegoInRoomMessageViewState extends State<ZegoInRoomMessageView> {
   static ScrollController scrollController = ScrollController();
-
-
-  StreamSubscription<dynamic>? streamSubscription;
-  var messagesNotifier = ValueNotifier<List<ZegoInRoomMessage>>([]);
   late ProfanityFilter filter;
 
   @override
@@ -65,27 +59,15 @@ class ZegoInRoomMessageViewState extends State<ZegoInRoomMessageView> {
     });
 
     scrollController = widget.scrollController ?? ScrollController();
-    streamSubscription = widget.stream.listen(onMessageUpdate);
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-
-    messagesNotifier.value = [];
-    streamSubscription?.cancel();
-  }
 
   @override
   Widget build(BuildContext context) {
    // messagesNotifier.value = widget.historyMessages;
     return Directionality(
         textDirection: ui.TextDirection.rtl,
-        child: ValueListenableBuilder<List<ZegoInRoomMessage>>(
-          valueListenable: messagesNotifier,
-          builder: (context, messageList, child) {
-            //todo  add value lester here
-            return  MediaQuery.removePadding(
+        child:  MediaQuery.removePadding(
                 context: context,
                 removeTop: true,
                 removeBottom: true,
@@ -102,94 +84,85 @@ class ZegoInRoomMessageViewState extends State<ZegoInRoomMessageView> {
                     ))
                           : BoxConstraints.loose(Size(650.zR, 550.zR,))
                           : BoxConstraints.loose(Size(650.zR, 435.zR,)),
-                      child: SingleChildScrollView(
-                        controller: scrollController,
-                        child: Column(
-                          children: [
-                            Row(
-                              children: [
-                                SizedBox(width: 50.zW, height: 50.zH, child: Image.asset(AssetsPath.iconApp)),
-                                SizedBox(
-                                  width: 10.zW,
-                                ),
-                                Expanded(
-                                  child: Container(
-                                    padding: EdgeInsets.all(10.zR),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(20.zR),
-                                      color: ColorManager.roomComment.withOpacity(0.3),
-                                    ),
-                                    child: Text(
-                                      overflow: TextOverflow.visible,
-                                      "${widget.room.roomRule}",
-                                      style: TextStyle(color: ColorManager.textRoomComment ,fontSize: 24.zSP),
-                                    ),
+                      child: MessagesBody(stream: widget.stream,itemBuilder: widget.itemBuilder,
+                        historyMessages: widget.historyMessages, scrollController: scrollController,
+                        room: widget.room,)
 
-                                  ),
-                                )
-                              ],
-                            ),
-                            SizedBox(height: 10.zH,),
-                            Row(
-                              children: [
-                                SizedBox(width: 50.zW, height: 50.zH,
-                                    child: Image.asset(AssetsPath.roomIntroMessageIcon)),
-                                SizedBox(
-                                  width: 10.zW,
-                                ),
-                                Expanded(
-                                  child: Container(
-                                    padding: EdgeInsets.all(10.zR),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(20.zR),
-                                      color: ColorManager.roomComment.withOpacity(0.3),
-                                    ),
-                                    child: Text(
-                                      overflow: TextOverflow.visible,
-                                      "${StringManager.roomIntro.tr()} \n ${widget.room.roomIntro}",
-                                      style: TextStyle(color: ColorManager.textRoomComment2 ,fontSize: 24.zSP),
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                            ListView.builder(
-                              shrinkWrap: true,
-                              // controller: _scrollController,
-                              physics: const NeverScrollableScrollPhysics(),
-                              // widget.scrollable ? null : const NeverScrollableScrollPhysics(),
-                              itemCount: messageList.length,
-                              itemBuilder: (context, index) {
-                                final message = messageList[index];
-                                if (filter.hasProfanity(message.message)) {
-                                  message.message = filter.censor(message.message);
-                                }
 
-                                return widget.itemBuilder.call(context, message, {});
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
+                      // SingleChildScrollView(
+                      //   controller: scrollController,
+                      //   child: Column(
+                      //     children: [
+                      //       Row(
+                      //         children: [
+                      //           SizedBox(width: 50.zW, height: 50.zH, child: Image.asset(AssetsPath.iconApp)),
+                      //           SizedBox(
+                      //             width: 10.zW,
+                      //           ),
+                      //           Expanded(
+                      //             child: Container(
+                      //               padding: EdgeInsets.all(10.zR),
+                      //               decoration: BoxDecoration(
+                      //                 borderRadius: BorderRadius.circular(20.zR),
+                      //                 color: ColorManager.roomComment.withOpacity(0.3),
+                      //               ),
+                      //               child: Text(
+                      //                 overflow: TextOverflow.visible,
+                      //                 "${widget.room.roomRule}",
+                      //                 style: TextStyle(color: ColorManager.textRoomComment ,fontSize: 24.zSP),
+                      //               ),
+                      //
+                      //             ),
+                      //           )
+                      //         ],
+                      //       ),
+                      //       SizedBox(height: 10.zH,),
+                      //       Row(
+                      //         children: [
+                      //           SizedBox(width: 50.zW, height: 50.zH,
+                      //               child: Image.asset(AssetsPath.roomIntroMessageIcon)),
+                      //           SizedBox(
+                      //             width: 10.zW,
+                      //           ),
+                      //           Expanded(
+                      //             child: Container(
+                      //               padding: EdgeInsets.all(10.zR),
+                      //               decoration: BoxDecoration(
+                      //                 borderRadius: BorderRadius.circular(20.zR),
+                      //                 color: ColorManager.roomComment.withOpacity(0.3),
+                      //               ),
+                      //               child: Text(
+                      //                 overflow: TextOverflow.visible,
+                      //                 "${StringManager.roomIntro.tr()} \n ${widget.room.roomIntro}",
+                      //                 style: TextStyle(color: ColorManager.textRoomComment2 ,fontSize: 24.zSP),
+                      //               ),
+                      //             ),
+                      //           )
+                      //         ],
+                      //       ),
+                      //       ListView.builder(
+                      //         shrinkWrap: true,
+                      //         // controller: _scrollController,
+                      //         physics: const NeverScrollableScrollPhysics(),
+                      //         // widget.scrollable ? null : const NeverScrollableScrollPhysics(),
+                      //         itemCount: messageList.length,
+                      //         itemBuilder: (context, index) {
+                      //           final message = messageList[index];
+                      //           if (filter.hasProfanity(message.message)) {
+                      //             message.message = filter.censor(message.message);
+                      //           }
+                      //
+                      //           return widget.itemBuilder.call(context, message, {});
+                      //         },
+                      //       ),
+                      //     ],
+                      //   ),
+                      // ),
                     ) ;
                   } ,
                 )
-            );
-          },
-        ));
+            ));
   }
 
-  void onMessageUpdate(List<ZegoInRoomMessage> messages) {
-    messagesNotifier.value = messages;
 
-    Future.delayed(const Duration(milliseconds: 100), () {
-      if (messagesNotifier.value.isEmpty) {
-        return;
-      }
-      if (ZegoInRoomMessageViewState.scrollController.position.maxScrollExtent - ZegoInRoomMessageViewState.scrollController.position.pixels < 270.zH ) {
-        scrollController.jumpTo(scrollController.position.maxScrollExtent);
-      }
-
-    });
-  }
 }
