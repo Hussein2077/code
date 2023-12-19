47bee5034790650b66a3c17283bdbb73d3bb9f46
R star <75912765+ohamedkhaled@users.noreply.github.com>
2023-12-19 18:12:08 +0200

    add

--- lib/features/room_audio/presentation/Room_Screen.dart
+++ lib/features/room_audio/presentation/Room_Screen.dart
@@ -54,6 +54,7 @@ import 'package:tik_chat_v2/zego_code_v3/zego_live_audio/src/components/audio_vi
 import 'package:tik_chat_v2/zego_code_v3/zego_live_audio/src/live_audio_room.dart';
 import 'package:tik_chat_v2/zego_code_v3/zego_live_audio/src/live_audio_room_config.dart';
 import 'package:tik_chat_v2/zego_code_v3/zego_live_audio/src/live_audio_room_defines.dart';
+import 'package:tik_chat_v2/zego_code_v3/zego_uikit/src/components/audio_video/media/player.dart';
 import 'package:tik_chat_v2/zego_code_v3/zego_uikit/src/components/message/message_input.dart';
 import 'package:tik_chat_v2/zego_code_v3/zego_uikit/src/services/defines/command.dart';
 import 'package:tik_chat_v2/zego_code_v3/zego_uikit/src/services/defines/user.dart';
@@ -878,29 +879,17 @@ class RoomScreenState extends State<RoomScreen> with TickerProviderStateMixin {
                 : -1
             ..hostSeatIndexes = [0]
             ..seatConfig = getSeatConfig()
-            ..viewbackground = ViewbackgroundWidget(room: widget.room,
-                roomDataUpdates: roomDataUpdates,
-                userBannerData: userBannerData,
-                superBox: superBox,
-                layoutMode: RoomScreen.layoutMode,
-                controllerMusice: controllerMusice,
-                animationControllerEntro: animationControllerEntro,
-                animationControllerGift: animationControllerGift,
-                yallowBanner: yallowBanner,
-                showYellowBanner: showYellowBanner,
-                userIntroData: userIntroData,
-                offsetAnimationEntro: offsetAnimationEntro,
-                yellowBannercontroller: yellowBannercontroller,
-                offsetAnimationYellowBanner: offsetAnimationYellowBanner,
-                yallowBannerSender: yallowBannerSender,
-                isPlural: isPlural,
-                controllerBanner: controllerBanner,
-                offsetAnimationBanner: offsetAnimationBanner,
-                luckGiftBannderController: luckGiftBannderController,
-                offsetLuckGiftAnimationBanner: offsetLuckGiftAnimationBanner,
-                showPopUp: showPopUp,
-                popUpData: popUpData,
-                durationKickout: durationKickout)
+            ..viewbackground = ZegoMediaPlayer(
+              size: Size(200,200),
+              enableRepeat: true,
+              canControl: true,
+              showSurface: true,
+              initPosition: Offset(
+                  100
+                  ,
+                  100
+              ),
+            )
 
             ..background = BackgroundWidget(room: widget.room,
                 layoutMode:RoomScreen.layoutMode, isHost: widget.isHost)
