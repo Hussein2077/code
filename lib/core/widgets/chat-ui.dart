5688b4f7ded754586e41c9a011093760688b31b3
Ahmed Aly <57324624+ahmed3670-art@users.noreply.github.com>
2024-01-17 18:51:37 +0200

    add

--- lib/core/widgets/chat-ui.dart
+++ lib/core/widgets/chat-ui.dart
@@ -1,6 +1,7 @@
 import 'package:cometchat_chat_uikit/cometchat_chat_uikit.dart';
 import 'package:cometchat_chat_uikit/cometchat_chat_uikit.dart' as cometchat;
 import 'package:flutter/material.dart';
+import 'package:tik_chat_v2/core/utils/config_size.dart';
 
 class ChatUi {
   Palette palette = Palette(
@@ -21,7 +22,7 @@ class ChatUi {
 
   cometchat.Typography typography = cometchat.Typography.fromDefault(
       name: FontStyle(
-          fontSize: 22,
+          fontSize: ConfigSize.defaultSize!*1.5,
           fontWeight: FontWeight.w400
       )
   );
